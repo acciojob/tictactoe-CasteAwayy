89a128b9e5a02495f8a7a5cf60e0bdf9cf89e396
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-09 02:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ const inputSection = document.getElementById('input-section');
 
         const winningCombinations = 
         [
-          [0,1,2],
-          [3,4,5],
-          [6,7,8],
-          [0,3,6],
-          [1,4,7],
-          [2,5,8],
-          [0,4,8],
-          [2,4,6]
+          [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
         ];
 
         function createBoard(){
           boardElement.innerHTML = '';
-          for(let i = 0; i < 9; i++){
+          for(let i = 1; i <= 9; i++){
             const cell = document.createElement('div');
             cell.id = Number(i);
             cell.addEventListener('click', () => handleCellClick(i));
