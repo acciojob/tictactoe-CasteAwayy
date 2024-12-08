3a9c33f2b5ae0dcd68e874d00453e27d152ad2f9
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-09 02:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -61,8 +61,8 @@ const inputSection = document.getElementById('input-section');
         }
 
         submitButton.addEventListener('click', () => {
-          const player1 = document.getElementById('player-1').value.trim();
-          const player2 = document.getElementById('player-2').value.trim();
+          const player1 = document.getElementById('player1').value.trim();
+          const player2 = document.getElementById('player2').value.trim();
           
           if(!player1 || !player2){
             alert('Please enter the name of both player');
