8eded3cc3ef00007267d5ec46f382abaa09ed47e
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-09 01:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-//your JS code here. If required.
+const inputSection = document.getElementById('input-section');
+        const gameSection = document.getElementById('game-section');
+        const boardElement = document.getElementById('board');
+        const messageElement = document.getElementById('message');
+        const submitButton = document.getElementById('submit');
+
+
+        let players = [];
+        let currentPlayerIndex = 0;
+        let board = Array(9).fill(null);
+
+        //winning combi
+
+        const winningCombinations = 
+        [
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+          [0,4,8],
+          [2,4,6]
+        ];
+
+        function createBoard(){
+          boardElement.innerHTML = '';
+          for(let i = 0; i < 9; i++){
+            const cell = document.createElement('div');
+            cell.id = i;
+            cell.addEventListener('click', () => handleCellClick(i));
+            boardElement.appendChild(cell);
+          }
+        }
+
+
+        function handleCellClick(index){
+          if(board[index] || checkWinner()) return;
+          
+          if(currentPlayerIndex === 0){
+            board[index] = 'X';
+          }else{
+            board[index] = 'O';
+          }
+          
+          document.getElementById(index).textContent = board[index];
+          if(checkWinner()){
+            messageElement.textContent = `${players[currentPlayerIndex]}, Congrats you won !`;
+          }else if(board.every(cell=>cell)){
+            messageElement.textContent = "It's a Draw";
+          }else{
+            currentPlayerIndex = 1 - currentPlayerIndex;
+            messageElement.textContent = `${players[currentPlayerIndex]}, You are up !`
+          }
+        }
+
+        function checkWinner(){
+          return winningCombinations.some(combination=>
+          combination.every(index => board[index] === (currentPlayerIndex === 0 ? 'X' : 'O'))
+          );
+        }
+
+        submitButton.addEventListener('click', () => {
+          const player1 = document.getElementById('player-1').value.trim();
+          const player2 = document.getElementById('player-2').value.trim();
+          
+          if(!player1 || !player2){
+            alert('Please enter the name of both player');
+            return;
+          }
+          
+          players = [player1, player2];
+          currentPlayerIndex = 0;
+          inputSection.style.display = 'none';
+          gameSection.style.display = 'block';
+          
+          createBoard();
+          
+        })
